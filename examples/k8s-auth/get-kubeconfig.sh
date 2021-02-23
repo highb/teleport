ad93ac9ba411c7dfe51bd11475e7cc640ac13bd2
Andrew Lytvynov <andrew@goteleport.com>
2021-02-23 10:44:40 -0800

    k8s: add merge-kubeconfigs.sh script
    This script merges multiple kubeconfigs created with `get-kubeconfig.sh`
    (or from anywhere else) into a single `merged-kubeconfig`.

--- examples/k8s-auth/get-kubeconfig.sh
+++ examples/k8s-auth/get-kubeconfig.sh
@@ -142,4 +142,7 @@ Copy the generated kubeconfig file to your Teleport Proxy server, and set the
 kubeconfig_file parameter in your teleport.yaml config file to point to this
 kubeconfig file.
 
+If you need access to multiple kubernetes clusters, you can generate additional
+kubeconfig files using this script and then merge them using merge-kubeconfigs.sh.
+
 Note: Kubernetes RBAC rules for Teleport were created, you won't need to create them manually."
